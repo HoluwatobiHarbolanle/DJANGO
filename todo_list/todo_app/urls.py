f4e02ca882c49ee22a6160a23d585e0962e213ec
Holuwatobi Harbolanle <bolanleoluwatobi844@gmail.com>
2024-08-13 22:38:15 +0100

    create, update, delete functions

--- todo_list/todo_app/urls.py
+++ todo_list/todo_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('task_list', views.task_lists, name='task')
+    path('task_list', views.task_lists, name='task_list'),
+    path('create_task', views.create_new, name='create_task'),
+    path('update_task/<int:task_id>', views.update_task, name='update_task'),
+    path('delete_task/<int:task_id>', views.delete_task, name='delete_task')
 ]
--- todo_list/todo_app/views.py
+++ todo_list/todo_app/views.py
@@ -9,3 +9,36 @@ def task_lists(request):
     tasks = Task.objects.all()
 
     return render(request, "task_list.html", {"tasks": tasks})
+
+def create_new(request):
+    if request.method == 'POST':
+        task = Task(
+            title = request.POST.get('title'),
+            complete = request.POST.get('status', 'off') == 'on',
+            due_date = request.POST.get('datetime')
+        )
+        task.save()
+        return redirect('task_list')
+    return render(request, 'create-task.html')
+
+def update_task(request, task_id):
+    task = Task.objects.get(id= task_id)
+    if request.method == 'POST':
+        task.title = request.POST.get('title'),
+        task.complete = request.POST.get('status'),
+        task.due_date = request.POST.get('datetime')
+        # task.complete = complete
+        if 'status' in request.POST:
+            task.complete = True
+        else:
+            task.complete = False
+        print(task.complete)
+        task.save()
+        return redirect('task_list')
+    return render(request, 'update_task.html', {'task': task})
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id = task_id)
+    task.delete()
+    return redirect('task_list')
+    # return render(request, 'task_list.html')
