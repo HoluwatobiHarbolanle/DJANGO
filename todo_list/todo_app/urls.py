613776b0abd29b5828f66cf4736a58a545738b5d
Holuwatobi Harbolanle <bolanleoluwatobi844@gmail.com>
2024-08-28 16:13:52 +0100

    CBVs implemented

--- todo_list/todo_app/urls.py
+++ todo_list/todo_app/urls.py
@@ -5,14 +5,20 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('',views.index, name='index'),
     path('register/', views.registration, name= 'register'),
-    # path('login', views.login, name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name = 'registration/logout.html'), name='logout'),
-    path('task_list/', views.task_lists, name='task_list'),
-    path('create_task/', views.create_new_task, name='create_task'),
-    path('update_task/<int:task_id>/', views.update_task, name='update_task'),
-    path('delete_task/<int:task_id>/', views.delete_task, name='delete_task'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name = 'registration/password_reset.html'), name='password_reset'),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name = 'registration/password_reset_done.html'), name='password_reset_done'),
-    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name = 'registration/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name = 'registration/password_reset_complete.html'), name='password_reset_complete')
+    path('login', views.login, name='login'),
+    path('account/logout/', auth_views.LogoutView.as_view(template_name = 'registration/logout.html'), name='logout'),
+    path('task_list', views.TaskList.as_view(), name='task_list'),
+    path('create_task/', views.CreateTask.as_view(), name='create_task'),
+    path('update_task/<int:pk>/', views.UpdateTask.as_view(), name='update_task'),
+    path('delete_task/<int:pk>/', views.DeleteTask.as_view(), name='delete_task'),
+
+    path('account/password_reset/', auth_views.PasswordResetView.as_view(template_name = 'registration/password_reset.html'), name='password_reset'),
+    path('account/password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name = 'registration/password_reset_done.html'), name='password_reset_done'),
+    path('account/password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name = 'registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('account/password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name = 'registration/password_reset_complete.html'), name='password_reset_complete')
 ]
+
+
+    # path('create_task/', views.create_new_task, name='create_task'),
+    # path('update_task/<int:task_id>/', views.update_task, name='update_task'),
+    # path('delete_task/<int:task_id>/', views.delete_task, name='delete_task'),
--- todo_list/todo_app/views.py
+++ todo_list/todo_app/views.py
@@ -1,43 +1,26 @@
+from typing import Any
+from django.forms import BaseModelForm
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 from .models import Task
 from django.contrib.auth.decorators import login_required, permission_required
 from django.db.models import Q
-# from django.contrib.auth.models import User
-from .forms import RegisterForm
+from django.contrib.auth.models import User
+from .forms import RegisterForm, TaskForm
 from django.contrib.auth import login
 from django.contrib.auth import logout as auth_logout
+from django.urls import reverse, reverse_lazy
+"""
+Class based views
+"""
+from django.views import View
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
+
 
 # Create your views here.
 def index(request):
     return render(request, "base.html")
-
-def login(request):
-        login(request)
-        return redirect('task_list')
-
-
-def logout_view(request):
-    if request.method == 'POST':
-        auth_logout(request)
-    return render(request, 'logout.html')
-
-    # return render(request, 'logout.html')
-# def registration(request):
-#     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         email = request.POST.get('email')
-#         password = request.POST.get('password1')
-#         confirm_password = request.POST.get('password2')
-#         if password == confirm_password:
-#             usr = User.objects.create_user(
-#                 username = username,
-#                 email = email,
-#                 password = password
-#             )
-#             return redirect('login')
-#         else:
-#             return render(request, 'registration.html', {'error': 'Passwords do not match.'})
-#     return render(request, 'registration.html')
 
 def registration(request):
     if request.method == 'POST':
@@ -52,44 +35,141 @@ def registration(request):
         return render(request, 'register.html', {'form': form})
 
 
-@login_required
-def task_lists(request):
-    u = request.user
-    # tasks = Task.objects.filter(Q(created_by = u) | Q(assigned_to = u))
-    tasks = (Task.objects.filter(created_by = u) or Task.objects.filter(assigned_to = u).distinct()).order_by('created')
+class TaskList(ListView):
+    model = Task, User
+    template_name = 'task_list.html'
+    context_object_name = 'tasks'
+    success_url = 'task_list'
 
-    return render(request, "task_list.html", {"tasks": tasks, 'user':u})
+    def get_queryset(self):
+        # if self.request.user.is_superuser:
+        #     return Task.objects.all()
+        # else:
+        usr = self.request.user
+        return (Task.objects.filter(created_by = usr) or Task.objects.filter(assigned_to = usr).distinct()).order_by('created')
+    def get_success_url(self):
+        return reverse('task_list')
 
-@login_required
-def create_new_task(request):
-    if request.method == 'POST':
-        task = Task.objects.create(
-            title = request.POST.get('title'),
-            complete = request.POST.get('status', 'off') == 'on',
-            due_date = request.POST.get('datetime'),
-            created_by = request.user,
-            assigned_to = request.user
-        )
-        return redirect('task_list')
-    return render(request, 'create-task.html')
+class CreateTask(LoginRequiredMixin, CreateView):
+    model = Task
+    template_name = 'create-task.html'
+    success_url = reverse_lazy('task_list')
+    form_class = TaskForm
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user or self.request.user.is_superuser 
+        form.instance.assigned_to =  self.request.user or self.request.user.is_supersuser
+        return super().form_valid(form)
+    def get_success_url(self) -> str:
+        return reverse_lazy('task_list')
+    
+    # def get(self, request):
+    #     task = Task.objects.create(
+    #     title = request.POST.get('title'),
+    #     complete = request.POST.get('status', 'off') == 'on',
+    #     due_date = request.POST.get('datetime'),
+    #     created_by = request.user,
+    #     assigned_to = request.user
+    #     )
+    #     return redirect('task_list')
+    
+    # def post(self, request):
+    #     return render(request, 'create-task.html')
 
-@permission_required('can_edit_task')
-def update_task(request, task_id):
-    task = Task.objects.get(id=task_id)
-    if request.method == 'POST':
-        task.title = request.POST.get('title') 
-        task.complete = request.POST.get('status') == 'on' 
-        task.due_date = request.POST.get('datetime')
-        task.save()
-        return redirect('task_list')
-    return render(request, 'update_task.html', {'task': task})
+class UpdateTask(UpdateView, UserPassesTestMixin):
+    model = Task
+    template_name = 'update_task.html'
+    form_class = TaskForm
+    success_url = reverse_lazy('task_list')
+
+    def get_success_url(self):
+        return reverse('task_list')
+    def test_func(self):
+        task = self.get_object()
+        return self.request.user == task.created_by or self.request.user.is_superuser
 
 
-@permission_required('can_delete_task')
-def delete_task(request, task_id):
-    task = Task.objects.get(id = task_id)
-    task.delete()
-    return redirect('task_list')
+
+class DeleteTask(DeleteView, UserPassesTestMixin):
+    model = Task
+    template_name = 'task_confirm_delete.html'
+    success_url=  reverse_lazy('task_list')
+    context_object_name = 'task'
+    def get_success_url(self):
+        return reverse('task_list')
+    def test_func(self):
+        task = self.get_object()
+        return self.request.user == task.created_by or self.request.user.is_superuser
+        
+
+# def login(request):
+#         login(request)
+#         return redirect('task_list')
+
+
+# def logout_view(request):
+#     if request.method == 'POST':
+#         auth_logout(request)
+#     return render(request, 'logout.html')
+
+#     # return render(request, 'logout.html')
+# # def registration(request):
+# #     if request.method == 'POST':
+# #         username = request.POST.get('username')
+# #         email = request.POST.get('email')
+# #         password = request.POST.get('password1')
+# #         confirm_password = request.POST.get('password2')
+# #         if password == confirm_password:
+# #             usr = User.objects.create_user(
+# #                 username = username,
+# #                 email = email,
+# #                 password = password
+# #             )
+# #             return redirect('login')
+# #         else:
+# #             return render(request, 'registration.html', {'error': 'Passwords do not match.'})
+# #     return render(request, 'registration.html')
+
+
+
+
+# @login_required
+# def task_lists(request):
+#     u = request.user
+#     # tasks = Task.objects.filter(Q(created_by = u) | Q(assigned_to = u))
+#     tasks = (Task.objects.filter(created_by = u) or Task.objects.filter(assigned_to = u).distinct()).order_by('-created')
+
+#     return render(request, "task_list.html", {"tasks": tasks, 'user':u})
+
+# @login_required
+# def create_new_task(request):
+#     if request.method == 'POST':
+#         task = Task.objects.create(
+#             title = request.POST.get('title'),
+#             complete = request.POST.get('complete', 'off') == 'on',
+#             due_date = request.POST.get('due_date'),
+#             created_by = request.user,
+#             assigned_to = request.user
+#         )
+#         return redirect('task_list')
+#     return render(request, 'create-task.html')
+
+# @permission_required('can_edit_task')
+# def update_task(request, task_id):
+#     task = Task.objects.get(id=task_id)
+#     if request.method == 'POST':
+#         task.title = request.POST.get('title') 
+#         task.complete = request.POST.get('complete') == 'on' 
+#         task.due_date = request.POST.get('due_date')
+#         task.save()
+#         return redirect('task_list')
+#     return render(request, 'update_task.html', {'task': task})
+
+
+# @permission_required('can_delete_task')
+# def delete_task(request, task_id):
+#     task = Task.objects.get(id = task_id)
+#     task.delete()
+#     return redirect('task_list')
     # return render(request, 'task_list.html')
 
 """
